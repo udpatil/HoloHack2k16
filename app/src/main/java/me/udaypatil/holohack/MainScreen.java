870ddf4c71ea20749b752c6cc40818b1c0723c52
udpatil <udpatil@gmail.com>
2016-12-02 22:45:13 -0500

    basic mapview working

--- app/src/main/java/me/udaypatil/holohack/MainScreen.java
+++ app/src/main/java/me/udaypatil/holohack/MainScreen.java
@@ -1,7 +1,10 @@
 package me.udaypatil.holohack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        Button mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent switchToMap = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(switchToMap);
+            }
+        });
     }
 }
